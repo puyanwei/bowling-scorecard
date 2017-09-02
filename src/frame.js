c289502f151b914376fde56d7a2fadd9ac9c6bb0
pyan83 <puyanwei@gmail.com>
2017-09-02 13:03:04 +0100

    created a bowl function as well as some scoring

--- src/frame.js
+++ src/frame.js
@@ -1,4 +1,14 @@
 var Frame = function() {
-
-
+  this.pins = 10;
 }
+
+Frame.prototype.bowl = function(knockedDown) {
+  if (knockedDown > 10) {
+    throw ("Cannot choose more then 10")
+  }
+  return this.pins = this.pins - knockedDown;
+};
+
+Frame.prototype.reset = function() {
+  this.pins = 10;
+};
