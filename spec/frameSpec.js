baea9ccdedd6b5e11e24585d4d69b1f0d5325b5f
pyan83 <puyanwei@gmail.com>
2017-09-15 11:35:23 +0100

    added automatic spare and strike recognition

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,22 +1,39 @@
-describe('Player', function() {
-
-  beforeEach(function() {
-    frame = new Frame();
-  });
+describe("Player", function() {
+  // beforeEach(function() {
+  //   frame = new Frame();
+  // });
 
   describe("#errorCheck", function() {
     it("throws an error if either entry is higher then 10", function() {
       expect(function() {
-        frame.errorCheck(11, 2);
+        frame = new Frame(11, 2);
       }).toThrow("entry cannot be higher then 10");
       expect(function() {
-        frame.errorCheck(5, 12);
+        frame = new Frame(5, 12);
       }).toThrow("entry cannot be higher then 10");
     });
     it("throws an error if the sum of entries is higher then 10", function() {
       expect(function() {
-        frame.errorCheck(6, 6);
+        frame = new Frame(6, 6);
       }).toThrow("the sum of entries cannot be higher then 10");
+    });
+  });
+
+  describe("#strikeCheck", function() {
+    it("sets strike to be true", function() {
+      frame = new Frame(10, 0);
+      expect(frame.strike).toBe(true);
+    });
+  });
+
+  describe("#spareCheck", function() {
+    it("sets spare to be true", function() {
+      frame = new Frame(5, 5);
+      expect(frame.spare).toBe(true);
+    });
+    it("sets spare to be true", function() {
+      frame = new Frame(0, 10);
+      expect(frame.spare).toBe(true);
     });
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,10 +1,12 @@
 var Frame = function(first, second) {
+  this.errorCheck(first, second);
   this.strike = false;
   this.spare = false;
   this.first = first;
   this.second = second;
-  this.totalFrame = this.first + this.second
-}
+  this.totalFrame = this.first + this.second;
+  this.tenPinsGoneCheck();
+};
 
 Frame.prototype.errorCheck = function(first, second) {
   if (first > 10 || second > 10) {
@@ -14,28 +16,20 @@ Frame.prototype.errorCheck = function(first, second) {
     throw "the sum of entries cannot be higher then 10";
   }
 };
-// Frame.prototype.firstBowl = function(first) {
-//   if (first > 10) {
-//     throw ("entry cannot be bigger then 10")
-//   }
-//   if (first == 10) {
-//     this.strike = true;
-//   }
-//   this.first = first;
-// };
-//
-// Frame.prototype.secondBowl = function(second) {
-//   if (second > 10) {
-//     throw ("entry cannot be bigger then 10")
-//   }
-//   if (this.first + second > 10) {
-//     throw ("entries cannot be a sum of more then 10")
-//   }
-//   if (second == 10) {
-//     this.spare = true;
-//   }
-//   if (this.first !== 10 && this.first + second == 10) {
-//     this.spare = true;
-//   }
-//   this.second = second;
-// };
+
+Frame.prototype.tenPinsGoneCheck = function() {
+  this.spareCheck();
+  this.strikeCheck();
+};
+
+Frame.prototype.spareCheck = function() {
+  if (this.first + this.second == 10) {
+    this.spare = true;
+  }
+};
+
+Frame.prototype.strikeCheck = function() {
+  if (this.first == 10) {
+    this.strike = true;
+  }
+};
