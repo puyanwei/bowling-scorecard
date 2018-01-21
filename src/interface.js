dc989c05aa0c2a6ecd79892733d90b990ee96500
Puyan Wei <puyanwei@gmail.com>
2018-01-21 22:39:40 +0000

    outputs number values to the console

--- src/interface.js
+++ src/interface.js
@@ -1,12 +1,10 @@
 $(document).ready(() => {
-  var player = new Player();
-  var frame = new Frame();
+  // var player = new Player();
+  // var frame = new Frame();
   var number = 0;
   var bowls = document.getElementsByTagName("span");
 
-  $(".button").on("click", () => {
-    console.log(number);
-    bowls[number].innerText = "hello";
-    number++;
+  $(".button").click(function() {
+    console.log($(this).val());
   });
 });
