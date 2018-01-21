6d7a720b38cec1df35099f3fbb0500f70aadc126
Puyan Wei <puyanwei@gmail.com>
2018-01-21 22:58:34 +0000

    swapped button locations, changed functions to be function expressions

--- src/interface.js
+++ src/interface.js
@@ -2,9 +2,9 @@ $(document).ready(() => {
   // var player = new Player();
   // var frame = new Frame();
   var number = 0;
-  var bowls = document.getElementsByTagName("span");
 
   $(".button").click(function() {
+    var bowls = document.getElementsByTagName("span");
     bowls[number].innerText = $(this).val();
     number++;
   });
