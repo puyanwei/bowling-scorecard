9cef15bc37c56b9856626331ed328a46f30e94af
Puyan Wei <puyanwei@gmail.com>
2018-01-22 00:10:26 +0000

    got a total score output working but only in frame one

--- src/interface.js
+++ src/interface.js
@@ -1,13 +1,21 @@
 $(document).ready(() => {
-  // var player = new Player();
-  // var frame = new Frame();
   var number = 0;
+  array = [];
 
   $(".button").click(function() {
-    var bowls = document.getElementsByTagName("span");
-    bowls[number].innerText = $(this).val();
+    var bowls = $("span")[number];
+    bowls.innerText = $(this).val();
+    calculateTotals(bowls.innerText);
     number++;
   });
 
-  var totals = function() {};
+  var calculateTotals = function(score) {
+    var total;
+    var number = parseInt(score);
+    array.push(number);
+    if (array.length % 2 == 0) {
+      total = array.reduce((a, b) => a + b, 0);
+    }
+    $(".total")[0].innerText = total;
+  };
 });
