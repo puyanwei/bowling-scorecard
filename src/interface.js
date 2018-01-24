39a87ab3b2a3c3beb2cb6f8d921ad69a676e7e48
Puyan Wei <puyanwei@gmail.com>
2018-01-24 16:25:17 +0000

    refactored to only call spans and total class once, created update function as controller to keep click event function skinny

--- src/interface.js
+++ src/interface.js
@@ -1,37 +1,48 @@
 $(document).ready(() => {
   var frameScoreIndex = 0;
   var frameTotalIndex = 0;
-  var allBowls = [];
+  var scoreCardArray = [];
+  var allSpans = $("span");
+  var totalClass = $(".total");
 
   $(".button").click(function() {
-    var bowlsSpan = $("span")[frameScoreIndex];
-    var buttonValue = parseInt($(this).val());
-    addScoreToPage(bowlsSpan, buttonValue);
-    hideButtonsIfSumOverTen(buttonValue);
-    calculateTotals(buttonValue);
-    frameScoreIndex++;
+    var bowlValue = parseInt($(this).val());
+    updateScore(bowlValue);
   });
+
+  var updateScore = function(bowlValue) {
+    addScoreToPage(bowlValue);
+    hideButtonsIfSumOverTen(bowlValue);
+    calculateTotals(bowlValue);
+    frameScoreIndex++;
+  };
+
+  var bowlsSpan = function() {
+    return allSpans[frameScoreIndex];
+  };
+
+  var totalSpan = function() {
+    return totalClass[frameTotalIndex];
+  };
 
   var calculateTotals = function(score) {
     var total;
-    console.log(score);
-    console.log(allBowls);
-
-    allBowls.push(score);
-    if (hasEvenIndex(allBowls)) {
-      total = sumArray(allBowls);
+    scoreCardArray.push(score);
+    if (hasEvenIndex(scoreCardArray)) {
+      total = sumArray(scoreCardArray);
       addTotalToPage(total);
       frameTotalIndex++;
       resetButtons();
     }
-    if (score === 10) {
-      calculateTotals(0);
-      console.log("is Ten");
+  };
+
+  var ifStrike = function(bowlValue) {
+    if (bowlValue === 10) {
     }
   };
 
-  var hideButtonsIfSumOverTen = function(buttonValue) {
-    var remainingPins = 10 - buttonValue;
+  var hideButtonsIfSumOverTen = function(bowlValue) {
+    var remainingPins = 10 - bowlValue;
     var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     array.forEach(function(value) {
@@ -53,11 +64,12 @@ $(document).ready(() => {
     return array.reduce((a, b) => a + b, 0);
   };
 
-  var addScoreToPage = function(bowlsSpan, buttonValue) {
-    bowlsSpan.innerText = buttonValue;
+  var addScoreToPage = function(bowlValue) {
+    bowlsSpan().innerText = bowlValue;
+    ifStrike(bowlValue);
   };
 
   var addTotalToPage = function(total) {
-    $(".total")[frameTotalIndex].innerText = total;
+    totalSpan().innerText = total;
   };
 });
