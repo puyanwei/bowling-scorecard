4f12107949399efb7bc448463ddcc6c9b5e3c7db
Puyan Wei <puyanwei@gmail.com>
2018-02-04 12:13:14 +0000

    added chai to dependencies, tests can use expect and should

--- test/intergration-test.js
+++ test/intergration-test.js
@@ -1,6 +1,10 @@
 const Browser = require("zombie");
+var chai = require("chai"),
+  expect = chai.expect,
+  should = chai.should();
 
-url = "http://localhost:8080/";
+var should = require("chai").should();
+var url = "http://localhost:8080/";
 
 describe("User visits page", function() {
   const browser = new Browser();
@@ -11,16 +15,25 @@ describe("User visits page", function() {
 
   describe("zombie js works", function() {
     it("title of page should be Bowling Scorecard", function() {
-      browser.assert.text("title", "Bowling Scorecard");
+      console.log(browser);
+      browser.text("Title").should.equal("Bowling Scorecard");
     });
   });
 
-  describe("elements exist on page", function() {
-    it("there is are containers on the page", function(done) {
-      browser.assert.element(".scorecard");
-      browser.assert.element(".frame-title-container");
-      browser.assert.element(".score-container");
-      done();
-    });
-  });
+  // describe("elements exist on page", function() {
+  //   it("there is are containers on the page", function(done) {
+  //     browser.assert.element(".scorecard");
+  //     browser.assert.element(".frame-title-container");
+  //     browser.assert.element(".score-container");
+  //     done();
+  //   });
+  // });
+  //
+  // describe("button outcomes", function() {
+  //   it("clicking a button shows it on the scorecard", function(done) {
+  //     browser.pressButton("1");
+  //     browser.text.assert("span", "1");
+  //     done();
+  //   });
+  // });
 });
