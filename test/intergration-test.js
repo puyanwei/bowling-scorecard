3a0f1906b3f343a9d94f5a04eedb0ebc7fad72f1
Puyan Wei <puyanwei@gmail.com>
2018-02-04 12:31:28 +0000

    test sees elements on page

--- test/intergration-test.js
+++ test/intergration-test.js
@@ -13,27 +13,18 @@ describe("User visits page", function() {
     return browser.visit(url);
   });
 
-  describe("zombie js works", function() {
+  describe("zombie js loads properly", function() {
     it("title of page should be Bowling Scorecard", function() {
-      console.log(browser);
       browser.text("Title").should.equal("Bowling Scorecard");
     });
   });
 
-  // describe("elements exist on page", function() {
-  //   it("there is are containers on the page", function(done) {
-  //     browser.assert.element(".scorecard");
-  //     browser.assert.element(".frame-title-container");
-  //     browser.assert.element(".score-container");
-  //     done();
-  //   });
-  // });
-  //
-  // describe("button outcomes", function() {
-  //   it("clicking a button shows it on the scorecard", function(done) {
-  //     browser.pressButton("1");
-  //     browser.text.assert("span", "1");
-  //     done();
-  //   });
-  // });
+  describe("elements exist on page", function() {
+    it("there is are containers on the page", function(done) {
+      browser.assert.element(".scorecard");
+      browser.assert.element(".frame-title-container");
+      browser.assert.element(".score-container");
+      done();
+    });
+  });
 });
